20cf6527760ceac09434c73db7ded2178d0d75b2
sahan-manusha <sahan.manusha123@gmail.com>
2024-05-09 23:12:14 +0530

    Create new activity for video

---
+++ app/src/main/java/com/coding/meet/videoplayer/Video.kt
@@ -0,0 +1,11 @@
+package com.coding.meet.videoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Video : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
